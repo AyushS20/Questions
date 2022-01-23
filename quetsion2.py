94e260d23af2fcec4a45b58ddc36454e25637313
AyushS20 <97532292+AyushS20@users.noreply.github.com>
2022-01-23 16:59:01 +0530

    Update quetsion2.py

--- quetsion2.py
+++ quetsion2.py
@@ -1,1 +1,11 @@
-
+# Taking the inputs for size of series 
+size = int(input("Enter the size of the series:  "))
+i = 1
+while(i <= size): # first loop.
+   j = i
+   while(j >= 1): # inner loop.
+       print(j, end = ' ')  # print the series.
+       j = j - 1
+   i = i + 1
+   print("") # for line changes.
+    
