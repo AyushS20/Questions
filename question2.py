fcffd7d9ee30136778bebd25a806f7d23b032994
AyushS20 <97532292+AyushS20@users.noreply.github.com>
2022-01-23 17:20:31 +0530

    Update question2.py

--- question2.py
+++ question2.py
@@ -1,3 +1,4 @@
+# Question 2 (a)
 # Taking the inputs for size of series 
 size = int(input("Enter the size of the series:  "))
 i = 1
@@ -9,3 +10,23 @@ while(i <= size): # first loop.
    i = i + 1
    print("") # for line changes.
     
+# Question 2 (b)
+# Asking the User for input of rows
+n = int(input("Enter no of rows: "))
+for i in range(1, n+1):
+    for j in range(1, n-i+1):
+        print(end=' ')
+    for j in range (1, i+ 1):
+        print(j, end='')
+    for j in range (i-1, 0, -1):
+        print(j, end='')
+    print()
+for i in range (1,n):
+    for j in range(1, i+1):
+        print(end=' ')
+    for j in range (1, n-i):
+        print(j, end='')
+    for j in range (n-i, 0, -1):
+        print(j, end='')
+    print()
+ 
