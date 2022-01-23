c874be1376916870ef1ffe693c48e4cf017e2786
AyushS20 <97532292+AyushS20@users.noreply.github.com>
2022-01-23 19:58:56 +0530

    Create question3.py

---
+++ question3.py
@@ -0,0 +1,21 @@
+import math
+print(math.pi)                       # Function 1 : Value of Pi
+print(math.e)                        # Function 2 : Value of e
+print(math.tau)                      # Function 3 : Value of tau
+print(math.radians(30))              # Function 3 : converting degree to radians
+print(math.degrees(math.pi/6))       # Function 4 : converting radians to degree
+print(math.sin(math.pi/4))           # Function 5: using sin function
+print(math.cos(math.pi/4))           # Function 6: using cos function
+print(math.tan(math.pi/4))           # Function 7: using tan function
+print(math.log(10))                  # Function 8: using log function to the base e
+print(math.log10(10))                # Function 9: using log function to the base 10
+print(math.exp(-3))                  # Function 10: raising e to the power of given number: Thsi can also be verified by math.e**10 
+print(math.pow(2,4))                 # Function 11: raising the first number to the power of second number
+print(math.sqrt(100))                # Function 12: Square root Function
+print(math.ceil(4.5867))             # Function 13: approximates the given number to the smallest integer, greater than or equal to the given floating point number
+print(math.floor(4.5687))            # function 14: returns the largest integer less than or equal to the given number
+print(math.inf > 10e108)
+print(-math.inf < -10e108)
+print(math.factorial(5))             # Function 15: Factorial Function
+print(math.gcd(15, 5))               # Function 16: Greatest Common Divisor
+print(math.fabs(-9.09372))           # Function 17: Absolute value of the Number
